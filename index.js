91650c8f864255bd2aae6e52c0e8cd5ec88986bd
“Ahmed <Ahmed.Abdirizaak@medieinstitutet.se>
2025-10-18 10:53:14 +0200

    Add Health Endpoint

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from "express";
 const app = express();
 
+app.get("/health", (_req, res) => {
+  res.status(200).json({
+    status: "OK",
+    message: "server health is OK",
+    success: true,
+  });
+});
+
 const PORT = 4040;
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
