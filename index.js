7a30ad038b7928834fdfb91c651c1d3cdcebd70f
“Ahmed <Ahmed.Abdirizaak@medieinstitutet.se>
2025-10-18 10:58:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ app.get("/health", (_req, res) => {
 
 app.get("/", (_req, res) => {
   res.status(200).json({
-    message: "Server is running on port",
-    PORT,
+    message: `Server is running on port ${PORT}`,
   });
 });
 
